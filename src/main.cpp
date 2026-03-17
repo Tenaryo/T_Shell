b360cf49c97a1af5093b8fb43be84a5f19e5f370
Tenaryo <tylertan@sjtu.edu.cn>
2026-03-18 00:46:05 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <string>
 
+void inline handle_not_found(string_view usrInput) {
+  std::cerr << usrInput << ": command not found\n";
+}
+
+void handle_input() {
+  std::string_view usrInput;
+  std::cin >> usrInput;
+  handle_not_found(usrInput);
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
-  std::string usrInput;
-  std::cin >> usrInput;
-  std::cerr << usrInput << ": command not found\n";
+  while (true) {
+    std::cout << "$ ";
+    handle_input();
+  }
 }
