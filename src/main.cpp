490a93f8a6d7447be3f7bd2ceddb1fc34661b412
Tenaryo <tylertan@sjtu.edu.cn>
2026-03-18 08:40:24 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 #include <string>
 #include <string_view>
+#include <sstream>
 #include <unordered_map>
 #include <functional>
+
+void cmd_type();
+void cmd_echo();
+void cmd_exit();
+
+static const std::unordered_map<std::string, std::function<void()>> commands {
+  { "type", cmd_type },
+  { "echo", cmd_echo },
+  { "exit", cmd_exit }
+};
+
+void handle_not_found(std::string_view command) {
+  std::cerr << command << ": command not found\n";
+}
+
+void cmd_type() {
+  std::string command;
+  std::getline(std::cin >> std::ws, command);
+  std::stringstream ss(command);
+
+  while (ss >> command) {
+    auto it = commands.find(command);
+    if (it != commands.end()) {
+      std::cout << it->first << " is a shell builtin\n";
+    } else {
+      handle_not_found(command);
+    }
+  }
+}
 
 void cmd_echo() {
   std::string s;
@@ -14,15 +44,7 @@ void cmd_exit() {
   exit(0);
 }
 
-void handle_not_found(std::string_view command) {
-  std::cerr << command << ": command not found\n";
-}
-
 void exec_shell() {
-  static const std::unordered_map<std::string, std::function<void()>> commands {
-    { "echo", cmd_echo },
-    { "exit", cmd_exit }
-  };
   std::cout << "$ ";
   std::string command;
   std::cin >> command;
